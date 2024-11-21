cb156b6e691b5b9977eb1b1a539f221e8dcab6f0
hospingg <hospingg@gmail.com>
2024-11-21 17:28:55 +0200

    Add modal details

--- js/main.js
+++ js/main.js
@@ -2,26 +2,39 @@ const modalAuth = document.querySelector('.modal-auth');
 const buttonAuth = document.querySelector('.button-auth');
 const buttonLogout = document.querySelector('.button-out');
 const loginInput = document.querySelector('#login');
+const passInput = document.querySelector('#password');
 const userName = document.querySelector('.user-name');
 const logInForm = document.querySelector('#logInForm');
 const closeModalButton = document.querySelector('.close-auth');
 const mes = document.querySelector('#ps')
 
+
 function toggleModal() {
    modalAuth.classList.toggle('is-open');
+   document.body.classList.toggle('remove-scroll')
+
+   loginInput.classList.remove('required');
+   passInput.classList.remove('required');
+   mes.classList.add('hide')
 }
 
 buttonAuth.addEventListener('click', toggleModal);
 
 closeModalButton.addEventListener('click', toggleModal);
 
+modalAuth.addEventListener('click', (event) => {
+   if (!event.target.closest('.auth-form')) {
+      toggleModal();
+   }
+});
 function logIn(event) {
    event.preventDefault();
 
    const login = loginInput.value.trim();
    if (login === "") {
       loginInput.classList.add('required');
-      mes.classList.toggle('hide')
+      passInput.classList.add('required');
+      mes.classList.remove('hide')
       return;
    }
 
